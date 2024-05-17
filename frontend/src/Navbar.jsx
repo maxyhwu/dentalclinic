bcd2c1d48092ad6a39ae8dd1540cf3ad1a09dd20
maxyhwu <b09705045@ntu.edu.tw>
2024-05-17 22:34:47 +0800

    modify navbar, update userpage with design and fix the addMember feature

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import {BrowserRouter as Router,Link,Routes,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Link,Routes,Route, NavLink } from 'react-router-dom';
 
 function Navbar() {
+
+  const username = 'Account';
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/user">User</Link>
-        </li>
-      </ul>
+    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dental Clinic Consumables System</span>
+        </Link>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+              <NavLink to="/" className={({isActive}) => "block py-2 px-3 md:p-0 " + isActive ? "text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" : "text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"}>Home</NavLink>
+            </li>
+            <li>|</li>
+            <li>
+              <NavLink to="/user" className={({isActive}) => "block py-2 px-3 md:p-0 " + isActive ? "text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" : "text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"}>{username}</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
--- frontend/src/UserPage.jsx
+++ frontend/src/UserPage.jsx
@@ -3,16 +3,19 @@ import Modal from 'react-modal';
 import Navbar from './Navbar';
 
 function UserPage() {
-  const showToggle = true;
-  const [status, setStatus] = useState(true);
   const [admin, setAdmin] = useState({'group_name': '', 'user_name': '', 'password': ''});
-  const user = { 'group_name': 'TEST', 'user_name': 'test_admin', 'password': 'test_admin' };
+  const user = { 'group_name': 'ADMIN', 'user_name': 'test_admin', 'password': 'test_admin' };
   const [showAdminPassword,setShowAdminPassword] = useState(false);
+  const [saveNewMember,setSaveNewMember] = useState(false);
   const [newMemberName,setNewMemberName] = useState('');
   const [newMemberPassword,setNewMemberPassword] = useState('');
   const [members,setMembers] = useState([]);
   const [modalIsOpen,setModalIsOpen] = useState(false);
   const [newNameExisted, setNewNameExisted] = useState(false);
+  const [membersFolded, setMembersFolded] = useState(true);
+  const showToggle = user.group_name === 'ADMIN';
+  const [status,setStatus] = useState(showToggle);
+  const [memberVisibilities,setMemberVisibilities] = useState({});
 
   //const api = "http://localhost:27017"
   const api = "https://dent-backend.onrender.com";
@@ -34,8 +37,7 @@ function UserPage() {
           throw new Error('Failed to add admin');
         });
       }
-
-      return response.json(); // This returns a promise
+      return response.json();
     })
     .then(data => {
       console.log('Response Data:',JSON.stringify(data));
@@ -45,52 +47,44 @@ function UserPage() {
     });
   };
 
-
-  useEffect(() => {
-    fetch(`${api}/user/?group_name=${admin.group_name}`)
-      .then(response => {
-        if(!response.ok) {
-          throw new Error('Failed to fetch group');
-        }
-        return response.json();
-      })
-      .then(data => {
-        const updatedMembers = data.filter(user => !user.is_admin).map((user, index) => ({ ...user, id: index + 1, password_visibility: false}));
-        setMembers(updatedMembers);
-        console.log('effect members:',members);
-      })
-      .catch(error => {
-        console.error('Error fetching group:', error);
-      });
-  },[]);
-
   const toggleAdminPasswordVisibility = () => {
     setShowAdminPassword(!showAdminPassword);
   };
 
   const toggleMemberPasswordVisibility = (id) => {
-    const updatedMembers = members.map(member => {
-      if(member.id === id) {
-        return { ...member, password_visibility: !member.password_visibility };
-      }
-      return member;
-    });
-    setMembers(updatedMembers);
-  }
+    setMemberVisibilities(prevState => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
+  };
 
-  const handleAddMember = () => {
-    setModalIsOpen(true);
-    if (members.length !== 0) {
-        if (members.some(member => member.user_name === newMemberName)) {
-          setNewNameExisted(true);
-        }
-        while (newNameExisted) {
-          if (!members.some(member => member.user_name === newMemberName)) {
-            setNewNameExisted(false);
-          }
-        };
-    };
-    fetch(`${api}/user/add_member/?group_name=${admin.group_name}&user_name=${admin.user_name}&password=${admin.password}`, {
+  const getMemberList = () => {
+    fetch(`${api}/user/?group_name=${user.group_name}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        const updatedMembers = data.filter(user => !user.is_admin).map((user,index) => ({...user,id: index + 1}));
+        setMembers(updatedMembers);
+      })
+      .catch(error => {
+        console.error('Error fetching member list:',error);
+      });
+  };
+
+  const handleSaveNewMember = () => {
+    //if (members.length !== 0) {
+    //    if (members.some(member => member.user_name === newMemberName)) {
+    //      setNewNameExisted(true);
+    //    };
+    //    if (setNewNameExisted) {
+    //      return;
+    //    }
+    //};
+    fetch(`${api}/user/add_member/?group_name=${user.group_name}&user_name=${newMemberName}&password=${newMemberPassword}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -100,26 +94,17 @@ function UserPage() {
         if(!response.ok) {
           throw new Error('Failed to add new member');
         }
-        setNewMemberName('');
-        setNewMemberPassword('');
-        return fetch(`${api}/user/?group_name=${admin.group_name}`);
-      })
-      .then(response => response.json())
-      .then(data => {
-        const updatedMembers = data.filter(user => !user.is_admin).map((user,index) => ({...user,id: index + 1, password_visibility: false}));
-        setMembers(updatedMembers);
       })
       .catch(error => {
         console.error('Error adding new member:',error);
       });
-      handleSaveNewMember();
-  };
 
-  const handleSaveNewMember = () => {
+    getMemberList();
     setNewMemberName('');
     setNewMemberPassword('');
     setModalIsOpen(false);
   };
+
 
   const handleDeleteMember = (id) => {
     const user = members.find(member => member.id === id);
@@ -133,72 +118,151 @@ function UserPage() {
         if(!response.ok) {
           throw new Error('Failed to delete user');
         }
-        return fetch(`${api}/user/?group_name=${admin.group_name}`);
       })
       .then(response => response.json())
-      .then(data => {
-        const updatedMembers = data.filter(user => !user.is_admin).map((user,index) => ({...user,id: index + 1, password_visibility: false}));
-        setMembers(updatedMembers);
-      })
       .catch(error => {
         console.error('Error deleting user:',error);
       });
+
+    getMemberList();
   };
+
+  useEffect(() => {
+    getMemberList();
+  });
 
   return (
     <>
-      { showToggle && <button onClick={() => setStatus(!status)}>Toggle Status</button> }
+      <Navbar />
       { status &&
         <>
-        <h1 className='font-bold bg-black text-white'>Welcome, IM Island!</h1>
-          <h2>Add Group</h2>
-          <label>Group Name: </label>
-          <input type="text" value={ admin.group_name } onChange={(e) => setAdmin({...admin, group_name:e.target.value})} /><br />
-          <label>Name: </label>
-          <input type="text" value={ admin.user_name } onChange={(e) => setAdmin({...admin,user_name: e.target.value})} /><br />
-          <label>Password: </label>
-          <input type="password" value={ admin.password } onChange={(e) => setAdmin({...admin,password: e.target.value})} /><br />
-          <button onClick={ addAdmin }>Save</button>
-        </>}
-      { !status &&
-      <>
-        <Navbar />
-        { console.log('members:',members) }
-        <h1>Welcome, { user.group_name }!</h1>
-        <div className='admin'>
-          <p>Username: { user.user_name }</p>
-          <p>Password: { showAdminPassword ? user.password : '********' }</p>
-          <button onClick={ toggleAdminPasswordVisibility }>
-            { showAdminPassword ? 'Hide Password' : 'Show Password' }
-          </button>
-          <br />
-          <button onClick={ handleAddMember }>Add Member</button>
-        </div>
-        <div className='member'>
-        { members.map(member => (
-          <div key={ member.id }>
-            <h2>Member Info</h2>
-            <p>Name: { member.user_name }</p>
-            <p>Password: { member.password_visibility ? member.user_password : '********' }</p>
-            <button onClick={ toggleMemberPasswordVisibility }>
-              { member.password_visibility ? 'Hide Password' : 'Show Password' }
-            </button>
-            { member.id !== 0 && (
-              <button onClick={() => handleDeleteMember(member.id)}>Delete</button>
-            )}
+          <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+            <h1 className="text-4xl mb-8 font-semibold">Welcome, IM Island!</h1>
+            <div className="bg-gray-50 p-8 rounded-lg shadow-md border border-gray-300">
+            <h2 className="text-2xl mb-4 font-semibold">Add Group</h2>
+              <label className="block mb-2">Group Name: </label>
+              <input
+                type="text"
+                value={admin.group_name}
+                onChange={(e) => setAdmin({...admin,group_name: e.target.value})}
+                className="w-full p-2 mb-4 border border-gray-300 rounded"
+              />
+              <label className="block mb-2">Admin Name: </label>
+              <input
+                type="text"
+                value={admin.user_name}
+                onChange={(e) => setAdmin({...admin,user_name: e.target.value})}
+                className="w-full p-2 mb-4 border border-gray-300 rounded"
+              />
+              <label className="block mb-2">Password: </label>
+              <input
+                type="password"
+                value={admin.password}
+                onChange={(e) => setAdmin({...admin,password: e.target.value})}
+                className="w-full p-2 mb-4 border border-gray-300 rounded"
+              />
+              <button
+                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-full"
+                onClick={addAdmin}
+              >
+                Save
+              </button>
+            </div>
           </div>
-        ))}
-        </div>
-        <Modal isOpen={ modalIsOpen } onRequestClose={() => setModalIsOpen(false)}>
-          <h2>Add Member</h2>
-          <label>Name: </label>
-          <input type="text" value={ newMemberName } onChange={(e) => setNewMemberName(e.target.value)} /><br />
-          { newNameExisted && <p>Name already exists. Please Enter a new name.</p> }
-          <label>Password: </label>
-          <input type="password" value={ newMemberPassword } onChange={(e) => setNewMemberPassword(e.target.value)} /><br />
-          <button onClick={ handleSaveNewMember }>Save</button>
-        </Modal>
-      </>}
+        </>
+}
+      { !status &&
+        <>
+          <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl mb-8 font-semibold">Welcome, {user.group_name}!</h1>
+            <div className="p-6 rounded-lg w-full max-w-4xl">
+              <div className="bg-gray-50 p-4 rounded-lg shadow-md mb-6">
+                <h2 className="text-2xl mb-4 font-semibold">Admin</h2>
+                <p className="text-lg">Username: {user.user_name}</p>
+                <p className="text-lg">Password: {showAdminPassword ? user.password : '********'}</p>
+                <div className="flex justify-end mt-2">
+                  <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-2' onClick={toggleAdminPasswordVisibility}>
+                    {showAdminPassword ? 'Hide Password' : 'Show Password'}
+                  </button>
+                  <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-2' onClick={() => setModalIsOpen(true)}>
+                    Add Member
+                  </button>
+                </div>
+              </div>
+              <div className="bg-gray-50 p-4 rounded-lg shadow-md">
+                <div className="flex items-center justify-between mb-4">
+                  <h2 className="text-2xl font-semibold">Members</h2>
+                  <button className="text-blue-500 font-semibold" onClick={() => {setMembersFolded(!membersFolded)}}>
+                    {membersFolded ? 'Expand' : 'Fold'}
+                  </button>
+                </div>
+                {!membersFolded &&
+                <>
+                  {members.map(member => (
+                    <div key={member.id} className="mb-4 p-4 bg-white rounded-lg shadow-md">
+                      <p className="text-lg">Name: {member.user_name}</p>
+                      <p className="text-lg">Password: {memberVisibilities[member.id] ? member.password : '********'}</p>
+                      <div className="flex justify-end mt-2">
+                        <button
+                          className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-2'
+                          onClick={() => toggleMemberPasswordVisibility(member.id)}
+                        >
+                          {memberVisibilities[member.id] ? 'Hide Password' : 'Show Password'}
+                        </button>
+                        {member.id !== 0 && (
+                          <button
+                            className='bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded ml-2'
+                            onClick={() => handleDeleteMember(member.id)}
+                          >
+                            Delete
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  ))}
+                </>}
+              </div>
+            </div>
+          </div>
+          <Modal
+            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+            isOpen={modalIsOpen}
+            onRequestClose={() => setModalIsOpen(false)}
+          >
+            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md">
+              <div className="flex justify-end">
+                <button className="text-red-500 font-semibold" onClick={() => setModalIsOpen(false)}>Close</button>
+              </div>
+              <h2 className="text-2xl mb-4">Add Member</h2>
+              <label className="block text-gray-700 text-sm font-bold mb-2">Name: </label>
+              <input
+                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                type="text"
+                value={newMemberName}
+                onChange={(e) => setNewMemberName(e.target.value)}
+              /><br />
+              {newNameExisted && <p className="text-red-500">Name already exists. Please enter a new name.</p>}
+              <label className="block text-gray-700 text-sm font-bold mb-2">Password: </label>
+              <input
+                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                type="password"
+                value={newMemberPassword}
+                onChange={(e) => setNewMemberPassword(e.target.value)}
+              /><br />
+              <button
+                className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-full mt-4'
+                onClick={handleSaveNewMember}
+              >
+                Save
+              </button>
+            </div>
+          </Modal>
+        </>
+
+}
+      <div>
+        {showToggle && <button className="fixed bottom-4 left-4 bg-red-500 text-white font-semibold py-2 px-4 rounded border hover:bg-white hover:text-red-500 hover:border-red-500" onClick={() => setStatus(!status)}>Toggle Status</button>}
+      </div>
     </>
   );
 }
